11419754dc12fc8e53712bf3e1525daa9222e52c
Paul Cohn <pscohn@gmail.com>
2015-02-16 17:54:40 -0500

    Ability to save and edit a document

--- routes/index.js
+++ routes/index.js
@@ -3,51 +3,70 @@ var router = express.Router();
 var redis = require('redis');
 var client = redis.createClient();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//router.get('/', function(req, res, next) {
+//  res.render('index', { title: 'Express' });
+//});
+
+
+function getDocumentIDs(callback) {
+    client.select('1', function() {
+        client.get('max-index', function(err, data) {
+            if ( err || data == null ) {
+                callback(0);
+            } else {
+                callback(Number(data.toString()));
+            }
+        });
+    });
+}
+
+router.get('/', function(req, res, next){
+    getDocumentIDs(function(result) {
+        console.log('got', result);
+        res.render('home', {maxIndex: result});
+    }); 
 });
 
-//router.get('/', function(req, res){
-//    console.log(req);
-//    res.sendFile(__dirname + req.path);
-//});
+router.get('/:id', function(req, res, next){
+    client.select('1', function(){
+        client.get('post-' + req.params.id, function(err, data) {
+            if (err || data == null) {
+                res.writeHead(404, {"Content-Type": "text/plain"});
+                res.write("404 Not Found\n");
+                res.end();
+            } else {
+                res.render('detail', {id: req.params.id, content: data.toString()});
+            }
+        });
+    });
+});
 
-//router.get('/:id', function(req, res, next){
-//    client.select('1', function(){
-//        client.get('post-' + req.params.id, function(err, data) {
-//            if (err || data == null) {
-//                res.writeHead(404, {"Content-Type": "text/plain"});
-//                res.write("404 Not Found\n");
-//                res.end();
-//                return;
-//            } else {
-//                res.send(data.toString());
-//                return
-//            }
-//        });
-//    });
-//});
-//
-//router.post('/', function(req, res, next){
-//    client.select('1', function(){
-//        client.get('max-index', function(err, data) {
-//            var maxindex;
-//            if (err || data == null) {
-//                console.log('no max-index found, returning 0');
-//                maxindex = 0;
-//            } else {
-//                console.log('max-index found =', data);
-//                maxindex = Number(data.toString());
-//            }
-//            var newindex = maxindex + 1;
-//            console.log('max-index is now', newindex);
-//            console.log('set post-' + String(newindex));
-//            client.set('max-index', newindex);
-//            client.set('post-' + String(newindex), req.body.content);
-//            res.redirect('/');
-//        });
-//    });
-//});
+router.post('/:id?', function(req, res, next){
+    client.select('1', function(){
+        if (req.params.id != null && req.params.id != undefined) {
+            client.set('post-' + req.params.id, req.body.content);
+            res.redirect('/' + req.params.id);
+        } else {
+            client.get('max-index', function(err, data) {
+                var maxindex;
+                if (err || data == null) {
+                    console.log('no max-index found, returning 0');
+                    maxindex = 0;
+                } else {
+                    console.log('max-index found =', data);
+                    maxindex = Number(data.toString());
+                }
+                var newindex = maxindex + 1;
+                console.log('max-index is now', newindex);
+                console.log('set post-' + String(newindex));
+                client.set('max-index', newindex);
+                client.set('post-' + String(newindex), req.body.content);
+                res.redirect('/');
+            });
+        }
+    });
+});
 
 module.exports = router;
